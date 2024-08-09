87eef18c5c5fd3fa72bd5b23ef01e2a76ed9449c
Mostafa Essam <mostafa.koraitem@gmail.com>
2024-08-10 01:21:27 +0300

    Install necessary dependencies for .NET Core API and Angular frontend

--- covid19-tracker-ui/src/app/app.module.ts
+++ covid19-tracker-ui/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartModule } from '@syncfusion/ej2-angular-charts';
+import { GridModule } from '@syncfusion/ej2-angular-grids';
+import { MapsModule } from '@syncfusion/ej2-angular-maps';
 
 @NgModule({
   declarations: [
@@ -10,6 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    GridModule,
+    ChartModule,
+    MapsModule,
     AppRoutingModule
   ],
   providers: [],
